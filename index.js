86595c5e7025ed799e02890923574e2448a414c6
Aleksandar Nedaković <aleksaleks123@gmail.com>
2020-12-29 16:18:10 +0100

    Prepare for Heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
+const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -89,7 +91,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
